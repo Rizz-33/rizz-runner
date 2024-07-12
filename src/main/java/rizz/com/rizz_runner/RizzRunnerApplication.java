97356e2a44158c52858c2ceebfaede44cb2ed67f
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-07-12 13:01:10 +0530

    first commit

---
+++ src/main/java/rizz/com/rizz_runner/RizzRunnerApplication.java
@@ -0,0 +1,13 @@
+package rizz.com.rizz_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizzRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizzRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rizz/com/rizz_runner/RizzRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package rizz.com.rizz_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RizzRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
