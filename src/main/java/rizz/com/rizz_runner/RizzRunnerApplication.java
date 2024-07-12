471d7ff49eff701797c5b57211d94fb26671e872
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-07-12 13:46:11 +0530

    feat: Message class

--- src/main/java/rizz/com/rizz_runner/RizzRunnerApplication.java
+++ src/main/java/rizz/com/rizz_runner/RizzRunnerApplication.java
@@ -3,11 +3,16 @@ package rizz.com.rizz_runner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import rizz.com.rizz_runner.classes.Message;
+
 @SpringBootApplication
 public class RizzRunnerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RizzRunnerApplication.class, args);
+
+		var message = new Message();
+		System.out.println(message.getMessage());
 	}
 
 }
---
+++ src/main/java/rizz/com/rizz_runner/classes/Message.java
@@ -0,0 +1,12 @@
+package rizz.com.rizz_runner.classes;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+
+    public String getMessage() {
+        return "Hello, Rizz Runner!";
+    }
+
+}
