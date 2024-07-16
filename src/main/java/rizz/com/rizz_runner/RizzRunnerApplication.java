205accdc744cc7915570836efac7f5fda3e2438c
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-07-16 11:46:06 +0530

    refactor: Logger info

--- src/main/java/rizz/com/rizz_runner/RizzRunnerApplication.java
+++ src/main/java/rizz/com/rizz_runner/RizzRunnerApplication.java
@@ -1,17 +1,20 @@
 package rizz.com.rizz_runner;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RizzRunnerApplication {
 
-	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(RizzRunnerApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(RizzRunnerApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        logger.info("Starting RizzRunner Successfully");
         
-		logger.info("Starting RizzRunner Successfully");
+        SpringApplication.run(RizzRunnerApplication.class, args);
+        
+        logger.info("RizzRunnerApplication has started");
     }
-
-
 }
