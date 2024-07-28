179e9032cc29e37c42b4329cbd91a5a71ceac1f7
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-07-28 19:49:43 +0530

    refeactor: Run func

--- src/main/java/rizz/com/rizz_runner/run/Run.java
+++ src/main/java/rizz/com/rizz_runner/run/Run.java
@@ -9,5 +9,26 @@ public record Run(
     LocalDateTime completedOn,
     Integer miles,
     Location location) {
+
+    public Run {
+        if (id == null) {
+            throw new IllegalArgumentException("id cannot be null");
+        }
+        if (title == null) {
+            throw new IllegalArgumentException("title cannot be null");
+        }
+        if (startedOn == null) {
+            throw new IllegalArgumentException("startedOn cannot be null");
+        }
+        if (completedOn == null) {
+            throw new IllegalArgumentException("completedOn cannot be null");
+        }
+        if (miles == null) {
+            throw new IllegalArgumentException("miles cannot be null");
+        }
+        if (location == null) {
+            throw new IllegalArgumentException("location cannot be null");
+        }
+    }
     
 }
